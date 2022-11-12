477922eed06a522a6c46b991e7b0fc74fb51cffc
unknown <ssalaudeen10@gmail.com>
2022-11-12 11:22:17 +0100

    Second commit

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -23,85 +23,88 @@ class _HomePageState extends State<HomePage> {
         elevation: 0.0,
         backgroundColor: Colors.white,
         toolbarHeight: 100,
-        flexibleSpace: Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 10.0,
-          ),
-          child: Row(
-            children: [
-              const SizedBox(
-                width: 30,
-              ),
-              const Image(
-                image: AssetImage(
-                  'images/Logo.png',
+        flexibleSpace: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 10.0,
+            ),
+            child: Row(
+              children: [
+                const SizedBox(
+                  width: 30,
                 ),
-              ),
-              const SizedBox(
-                width: 350,
-              ),
-              TextButton(
-                onPressed: () {
-                  appBarLogic.onClicked(link: Links.service);
-                },
-                child: Text(
-                  'Service',
-                  style: appBarTextStyle,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  appBarLogic.onClicked(link: Links.team);
-                },
-                child: Text(
-                  'Team',
-                  style: appBarTextStyle,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  appBarLogic.onClicked(link: Links.recentWorks);
-                },
-                child: Text(
-                  'Recent works',
-                  style: appBarTextStyle,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  appBarLogic.onClicked(link: Links.portfolio);
-                },
-                child: Text(
-                  'Portfolio',
-                  style: appBarTextStyle,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  appBarLogic.onClicked(link: Links.whyUs);
-                },
-                child: Text(
-                  'Why us',
-                  style: appBarTextStyle,
-                ),
-              ),
-              GestureDetector(
-                onTap: (() => appBarLogic.onClicked(link: Links.contact)),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: green, borderRadius: BorderRadius.circular(10)),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                  child: Text(
-                    'Contact',
-                    style: GoogleFonts.ubuntu(
-                      color: Colors.white,
-                    ),
+                const Image(
+                  image: AssetImage(
+                    'images/Logo.png',
                   ),
                 ),
-              )
-            ],
+                const SizedBox(
+                  width: 350,
+                ),
+                TextButton(
+                  onPressed: () {
+                    appBarLogic.onClicked(link: Links.service);
+                  },
+                  child: Text(
+                    'Service',
+                    style: appBarTextStyle,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    appBarLogic.onClicked(link: Links.team);
+                  },
+                  child: Text(
+                    'Team',
+                    style: appBarTextStyle,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    appBarLogic.onClicked(link: Links.recentWorks);
+                  },
+                  child: Text(
+                    'Recent works',
+                    style: appBarTextStyle,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    appBarLogic.onClicked(link: Links.portfolio);
+                  },
+                  child: Text(
+                    'Portfolio',
+                    style: appBarTextStyle,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    appBarLogic.onClicked(link: Links.whyUs);
+                  },
+                  child: Text(
+                    'Why us',
+                    style: appBarTextStyle,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: (() => appBarLogic.onClicked(link: Links.contact)),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: green, borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 15),
+                    child: Text(
+                      'Contact',
+                      style: GoogleFonts.ubuntu(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
